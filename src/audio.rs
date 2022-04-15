6af32b557404af8c13e21fc6ccaaef05ddc36a17
Andreas Neukoetter <andreas@omni-mad.com>
2022-04-15 17:55:05 +0200

    : Fix version. - Remove commented out code.

--- src/audio.rs
+++ src/audio.rs
@@ -13,37 +13,3 @@
 	mod audio_stub;
 #[cfg(use_stub)]
 	pub use audio_stub::AudioStub as Audio;
-	
-/*
-#[cfg(all(target_os = "macos", feature = "use_apple"))]
-mod audio_apple;
-#[cfg(all(target_os = "macos", feature = "use_apple"))]
-pub use audio_apple::AudioApple as Audio;
-
-#[cfg(all(target_os = "macos", feature = "use_miniaudio"))]
-mod audio_miniaudio;
-#[cfg(all(target_os = "macos", feature = "use_miniaudio"))]
-pub use audio_miniaudio::AudioMiniaudio as Audio;
-
-#[cfg(all(target_os = "macos", not( any( feature = "use_apple", feature= "use_miniaudio"  ) ) ) )]
-mod audio_stub;
-#[cfg(all(target_os = "macos", not( any( feature = "use_apple", feature= "use_miniaudio"  ) ) ) )]
-pub use audio_stub::AudioStub as Audio;
-
-
-#[cfg(all(target_os = "windows", not( any( feature= "use_miniaudio"  ) ) ) )]
-mod audio_stub;
-#[cfg(all(target_os = "windows", not( any( feature= "use_miniaudio"  ) ) ) )]
-pub use audio_stub::AudioStub as Audio;
-
-#[cfg(all(target_os = "windows", feature = "use_miniaudio"))]
-mod audio_miniaudio;
-#[cfg(all(target_os = "windows", feature = "use_miniaudio"))]
-pub use audio_miniaudio::AudioMiniaudio as Audio;
-
-#[cfg(target_os = "linux")]
-mod audio_stub;
-#[cfg(target_os = "linux")]
-pub use audio_stub::AudioStub as Audio;
-*/
-
--- src/music.rs
+++ src/music.rs
@@ -12,28 +12,3 @@
 	mod music_stub;
 #[cfg(use_stub)]
 	pub use music_stub::MusicStub as Music;
-
-/*
-#[cfg(all(target_os = "macos", feature = "use_apple"))]
-mod music_apple;
-#[cfg(all(target_os = "macos", feature = "use_apple"))]
-pub use music_apple::MusicApple as Music;
-*/
-
-/*
-#[cfg(all(target_os = "macos", not( feature = "use_apple" ) ))]
-mod music_stub;
-#[cfg(all(target_os = "macos", not( feature = "use_apple" ) ))]
-pub use music_stub::MusicStub as Music;
-*/
-/*
-#[cfg(target_os = "windows")]
-mod music_stub;
-#[cfg(target_os = "windows")]
-pub use music_stub::MusicStub as Music;
-
-#[cfg(target_os = "linux")]
-mod music_stub;
-#[cfg(target_os = "linux")]
-pub use music_stub::MusicStub as Music;
-*/
