8a69023051029e1dc2f4c3ef4b147458ae83179f
Andreas Neukoetter <andreas@omni-mad.com>
2022-04-15 17:48:32 +0200

    : Improve cfg handling. : Make packages optional as needed. : Update packages.

--- src/audio.rs
+++ src/audio.rs
@@ -1,4 +1,20 @@
 
+#[cfg(use_apple)]
+	mod audio_apple;
+#[cfg(use_apple)]
+	pub use audio_apple::AudioApple as Audio;
+
+#[cfg(use_miniaudio)]
+	mod audio_miniaudio;
+#[cfg(use_miniaudio)]
+	pub use audio_miniaudio::AudioMiniaudio as Audio;
+
+#[cfg(use_stub)]
+	mod audio_stub;
+#[cfg(use_stub)]
+	pub use audio_stub::AudioStub as Audio;
+	
+/*
 #[cfg(all(target_os = "macos", feature = "use_apple"))]
 mod audio_apple;
 #[cfg(all(target_os = "macos", feature = "use_apple"))]
@@ -29,3 +45,5 @@ pub use audio_miniaudio::AudioMiniaudio as Audio;
 mod audio_stub;
 #[cfg(target_os = "linux")]
 pub use audio_stub::AudioStub as Audio;
+*/
+
