88d3c7f5c943a4142b07b030ec125f51b11b8e5d
Andreas Neukoetter <andreas@omni-mad.com>
2023-01-24 22:52:53 +0100

    + Add (dummy) debug formatter for AudioBackend.

--- src/audio_backend.rs
+++ src/audio_backend.rs
@@ -11,3 +11,9 @@ where
 	fn play_sound(&mut self, _name: &str);
 	fn load_music_native(&mut self, fileloader: &mut F, filename: &str) -> bool;
 }
+
+impl<F> core::fmt::Debug for dyn AudioBackend<F> {
+	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+		write!(f, "[dyn AudioBackend]")
+	}
+}
