7bdffff883f9bab3569d169f82e40d3e383c3869
Andreas Neukoetter <andreas@omni-mad.com>
2023-01-24 22:41:03 +0100

    ! Add missing files.

---
+++ src/audio_backend.rs
@@ -0,0 +1,13 @@
+use crate::FileLoader;
+
+pub trait AudioBackend<F>
+where
+	F: FileLoader,
+{
+	fn start(&mut self) {}
+	fn load_sound_bank(&mut self, _fileloader: &mut F, _filename: &str);
+	fn update(&mut self) -> f64;
+	fn play_music(&mut self);
+	fn play_sound(&mut self, _name: &str);
+	fn load_music_native(&mut self, fileloader: &mut F, filename: &str) -> bool;
+}
